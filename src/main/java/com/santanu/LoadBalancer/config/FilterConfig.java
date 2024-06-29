e3e9b16e3ad97a6f5eaa818814fb39ff8b01c1ca
code-with-santanu <santanupal003@gmail.com>
2024-06-30 04:04:21 +0530

    filter registered

---
+++ src/main/java/com/santanu/LoadBalancer/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.santanu.LoadBalancer.config;
+
+import com.santanu.LoadBalancer.requestHandler.RequestForwardFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+
+// This class register the custom filter(RequestForwardFilter) in the spring bean
+@Configuration
+public class FilterConfig {
+
+    public FilterRegistrationBean<RequestForwardFilter> loggingFilter(){
+        FilterRegistrationBean<RequestForwardFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestForwardFilter());
+        registrationBean.addUrlPatterns("/lb/*");
+
+        return registrationBean;
+    }
+}
