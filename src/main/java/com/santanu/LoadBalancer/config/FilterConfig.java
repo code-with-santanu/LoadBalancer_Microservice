72acdf9493845fcb6f2b148abdd2bcc5cdafd1d3
code-with-santanu <santanupal003@gmail.com>
2024-07-03 13:35:08 +0530

    filter config error fixed

--- src/main/java/com/santanu/LoadBalancer/config/FilterConfig.java
+++ src/main/java/com/santanu/LoadBalancer/config/FilterConfig.java
@@ -2,12 +2,14 @@ package com.santanu.LoadBalancer.config;
 
 import com.santanu.LoadBalancer.requestHandler.RequestForwardFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // This class register the custom filter(RequestForwardFilter) in the spring bean
 @Configuration
 public class FilterConfig {
 
+    @Bean
     public FilterRegistrationBean<RequestForwardFilter> loggingFilter(){
         FilterRegistrationBean<RequestForwardFilter> registrationBean = new FilterRegistrationBean<>();
 
