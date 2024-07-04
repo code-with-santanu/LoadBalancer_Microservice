5557c197e7ef8e78e5dc4e23f89f61ea1a28fe76
code-with-santanu <santanupal003@gmail.com>
2024-07-04 23:42:17 +0530

    exception handling for data validation is done

---
+++ src/main/java/com/santanu/LoadBalancer/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.santanu.LoadBalancer.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp) {
+
+        Map<String,String> responseObj = new HashMap<>();
+
+        // Bind all the error messages in the responseObj
+        exp.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+
+            responseObj.put(fieldName,message);
+        });
+
+
+        return new ResponseEntity<Map<String,String>>(responseObj,HttpStatus.BAD_REQUEST);
+    }
+}
