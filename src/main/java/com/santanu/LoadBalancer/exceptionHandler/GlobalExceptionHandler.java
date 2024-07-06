a956913b732f57dee8bb7293bf4550e1b3420543
code-with-santanu <santanupal003@gmail.com>
2024-07-06 17:29:09 +0530

    AccessDenied exception handler added

--- src/main/java/com/santanu/LoadBalancer/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/santanu/LoadBalancer/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.santanu.LoadBalancer.exceptionHandler;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,4 +31,33 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<Map<String,String>>(responseObj,HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(HttpServletRequest request, AccessDeniedException ex) {
+        String path = request.getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+        errorResponse.setPath(path);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    // To handle other type of exception
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<ErrorResponse> handleGeneralException(Exception exp) {
+//        // Define custom response object
+//        ErrorResponse error = new ErrorResponse();
+//
+//        // Set the values
+//        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+//        error.setMessage(exp.getMessage());
+//        error.setTimeStamp(System.currentTimeMillis());
+//
+//        // Return the response object
+//        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 }
